3d8f3ca6496ac5e11ee42104ec7bd9a308a9e23e
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-06-26 09:13:13 +0300

    Initial commit
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

---
+++ app/src/main/java/styx/morse/Main.java
@@ -0,0 +1,63 @@
+package styx.morse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main extends Activity {
+    private TextView text_result;
+    private Button button_clear;
+    private Button button_space;
+    private Button button_backspace;
+    private Button button_copy;
+    private Button button_dot;
+    private Button button_dash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text_result = (TextView) findViewById(R.id.text_result);
+        button_clear = (Button) findViewById(R.id.button_clear);
+        button_copy = (Button) findViewById(R.id.button_clear);
+        button_space = (Button) findViewById(R.id.button_clear);
+        button_backspace = (Button) findViewById(R.id.button_clear);
+        button_dot = (Button) findViewById(R.id.button_clear);
+        button_dash = (Button) findViewById(R.id.button_clear);
+
+//        button_clear.setOnClickListener(new OnClickListener() {
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+
+    public void buttonClearHandler(View view) {
+       text_result.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

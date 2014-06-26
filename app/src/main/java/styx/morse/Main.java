373cebf60d2057f5cc6652a1e06b205f3c14edb4
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-06-26 09:19:59 +0300

    Copy to clipboard button
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- app/src/main/java/styx/morse/Main.java
+++ app/src/main/java/styx/morse/Main.java
@@ -1,6 +1,8 @@
 package styx.morse;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,10 +13,8 @@ import android.widget.TextView;
 
 public class Main extends Activity {
     private TextView text_result;
-    private Button button_clear;
     private Button button_space;
     private Button button_backspace;
-    private Button button_copy;
     private Button button_dot;
     private Button button_dash;
 
@@ -24,8 +24,6 @@ public class Main extends Activity {
         setContentView(R.layout.activity_main);
 
         text_result = (TextView) findViewById(R.id.text_result);
-        button_clear = (Button) findViewById(R.id.button_clear);
-        button_copy = (Button) findViewById(R.id.button_clear);
         button_space = (Button) findViewById(R.id.button_clear);
         button_backspace = (Button) findViewById(R.id.button_clear);
         button_dot = (Button) findViewById(R.id.button_clear);
@@ -40,6 +38,12 @@ public class Main extends Activity {
 
     public void buttonClearHandler(View view) {
        text_result.setText("");
+    }
+
+    public void buttonCopyHandler(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Decoded Morse", text_result.getText());
+        clipboard.setPrimaryClip(clip);
     }
 
     @Override
